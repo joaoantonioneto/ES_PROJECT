b877f85a2a216cd53717506ac0e88dec03759cb3
remoteEnterprise <reremoteenterprise@gmail.com>
2016-10-20 01:10:10 -0300

    New Class "Person"

--- src/br/ufpb/es/Joao.java
+++
@@ -1,7 +0,0 @@
-package br.ufpb.es;
-
-public class Joao {
-	private int n = 10;
-	private String string = "String";
-	//Comentário Teste.
-}
---
+++ src/br/ufpb/es/Person.java
@@ -0,0 +1,76 @@
+package br.ufpb.es;
+
+public class Person {
+	private String firstName;
+	private String lastName;
+	private int idade;
+	
+	public Person(String firstName, String lastName, int idade){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idade = idade;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + idade;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (idade != other.idade)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", idade=" + idade + "]";
+	}
+}
